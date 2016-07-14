099358fd5214d9c23b99d46af9358b0cf38a7287
man-ito-ba <pierre.m.global@gmail.com>
2016-07-14 10:39:35 +0200

    Élimination des combos en trop
    Led verte, rouge.

--- projet_express.ino
+++ projet_express.ino
@@ -8,23 +8,21 @@
 /*Déclarations*/
 //matériel
 // source d'entrée en volt
-const int Entree_V = A0;
+const int Entree_V = A3;
 int Entree_reelle;	// Ce que le potard va vraiment envoyer
-int V_entree;		// Les limites imposées par le produit (une entrée entre 0 et 1,86 V) ; avec cette variable, qu'importe l'entrée en volts réelle, tu auras toujours le bon ratio pour déclencher les différents élements (en dessous ou au dessus de 100V)
+int V_entree;		// Variable utilisée dans la fonction map(), représentant les limites imposées par le produit (une entrée entre 0 et 1,86 V) ; avec cette variable, qu'importe l'entrée en volts réelle, tu auras toujours le bon ratio pour déclencher les différents élements (en dessous ou au dessus de 100V).
 
 // Potar pour modifier le PWM
-const int Potar_PWM = A1;	// Potar qui fait varier la fréquence du signal PWM
-int Valeur_PWM;
+const int Potar_PWM = A4;	// Potar qui fait varier la fréquence du signal PWM
+int Valeur_PWM;				// Variable mémorisant la valeur du potar PWM
 
 // Ce qui sera allumé si V >= 1
-const int LedVerte = 2;
-const int No_220_24 = 3;			// pin pour le No_220 et No_24
-const int Lampe_Compteur_220 = 4;	// pin pour la lampe et le compteur
-const int Sortie_PWM = 5;
+const int No220_24 = A0;				// pin pour le No_220 et No_24
+const int Lampe_Compteur_220 = 10;		// pin pour la lampe et le compteur
+const int Sortie_PWM = 11;				// pin pour la sortie PWM, cette sortie 11 est adaptée car elle peut être utilisée pour du PWM
 // Ce qui sera déclenché si V < 1
-const int LedRouge = 7;
-const int NF_220_24 = 8;						// pin pour le NF_220 et NF_24
-const int Buzzer_Lampe_24_MOC_304X_3 = 9;		// pin pour le Buzzer, la Lampe_24, et le MOC 304X 3
+const int NF_220_24 = 12;						// pin pour le NF_220 et NF_24
+const int Buzzer_Lampe_24_MOC_304X_3 = 13;		// pin pour le Buzzer, la Lampe_24, et le MOC 304X 3
 
 //pratique
 const int Sec=1000; //Une variable qui me permet de compter en secondes dans le reste du programme :)
@@ -32,13 +30,15 @@ const int Sec=1000; //Une variable qui me permet de compter en secondes dans le 
 
 void setup()
 {
-	for(int i=2; i<9; i++)
+	for(int i=10; i<14; i++)
 	{ // initialisation de l'ensemble des pins liés en sortie (sauf pour V_entrée)
 	    pinMode(i, OUTPUT);
 	    digitalWrite(i, LOW);
 	}
-	pinMode(Entree_V, INPUT); // initialisation du matos d'entrée qui déclenche le reste
-	pinMode(Potar_PWM, INPUT);
+	pinMode(No220_24, OUTPUT);
+	digitalWrite(No220_24, LOW);
+	pinMode(Entree_V, INPUT); 					// initialisation du matos d'entrée qui déclenche le reste et simule l'entrée en Volts
+	pinMode(Potar_PWM, INPUT);					// initialisation du potar utilisé pour régler le PWM
 	V_entree, Entree_reelle, Valeur_PWM = 0;
 }
 
@@ -59,24 +59,21 @@ void loop()
 
 void Zero_a_Un()
 {
-	for(int i=2; i<6; i++){
-	    digitalWrite(i, LOW);
-	}
-	for(int j=7; j<10; j++){
-	    digitalWrite(j, HIGH);
-	}
+	digitalWrite(No220_24, LOW);
+	digitalWrite(Lampe_Compteur_220, LOW);
+	digitalWrite(Sortie_PWM, LOW);
+
+	digitalWrite(NF_220_24, HIGH);
+	digitalWrite(Buzzer_Lampe_24_MOC_304X_3, HIGH);
 }
 
 void Un_et_Plus()
 {
-	for(int i=3; i<6; i++)
-	{
-	    digitalWrite(i, HIGH);
-	}
-	for(int j=7; j<10; j++)
-	{
-	    digitalWrite(j, LOW);
-	}
-	int Valeur_PWM = analogRead(Potar_PWM);
-	analogWrite(LedVerte, Valeur_PWM/4);
+	digitalWrite(No220_24, HIGH);
+	digitalWrite(Lampe_Compteur_220, HIGH);
+	Valeur_PWM = analogRead(Potar_PWM);
+	analogWrite(Sortie_PWM, Valeur_PWM/4);
+
+	digitalWrite(NF_220_24, LOW);
+	digitalWrite(Buzzer_Lampe_24_MOC_304X_3, LOW);
 }
