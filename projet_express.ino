526408ca4b56edb6740df2d32f20bc9bfb2e3bd8
man-ito-ba <pierre.m.global@gmail.com>
2016-09-28 10:36:56 +0200

    MAJ pour RER
    On retourne à une config avec l’ensemble des composants commandés
    directement au lieu de faire des montages.

--- projet_express.ino
+++ projet_express.ino
@@ -1,79 +1,102 @@
-// Adresse du montage sur 123D.circuits.io où tu trouveras l'ensemble des composants nommés ! Pour cela, clique simplement sur chaque élément :)
-	// https://circuits.io/circuits/2435021
-// Adresse du plan sur Sketchboard
-	// https://sketchboard.me/Pz0gaDj1ZIQG
-// Adresse des MAJ que tu m'as donné sur Asana pour effectuer cette nouvelle version :
-	// https://app.asana.com/0/inbox/89529756025108/151877453969690/154799563809514
+int Entree_V = A0;		//entrée du signal principal
+int Entree_reelle;
+int V_entree; 
 
-/*Déclarations*/
-//matériel
-// source d'entrée en volt
-const int Entree_V = A3;
-int Entree_reelle;	// Ce que le potard va vraiment envoyer
-int V_entree;		// Variable utilisée dans la fonction map(), représentant les limites imposées par le produit (une entrée entre 0 et 1,86 V) ; avec cette variable, qu'importe l'entrée en volts réelle, tu auras toujours le bon ratio pour déclencher les différents élements (en dessous ou au dessus de 100V).
+int Entree_Pot = A1;	// entrée du potard
+int Valeur_PWM;
 
-// Potar pour modifier le PWM
-const int Potar_PWM = A4;	// Potar qui fait varier la fréquence du signal PWM
-int Valeur_PWM;				// Variable mémorisant la valeur du potar PWM
+// ===== Ce qui sera allumé si V >= 1
+int Compt_LP   = 2;		// pin pour le MOC COMPTEUR + LAMPE AC
+int No230      = 3;		// pin pour le MOC No230V
+int LEDvt      = 4;		// pin pour led verte
+int No24       = 5; 	// pin pour le 4N35 NO 24V
+int Sortie_PWM = 6; 	// pin6 pour la sortie PWM
+int Gnd        = 7; 	// pin pour couper le Gnd du ventilo et du bandeau de leds
 
-// Ce qui sera allumé si V >= 1
-const int No220_24 = A0;				// pin pour le No_220 et No_24
-const int Lampe_Compteur_220 = 10;		// pin pour la lampe et le compteur
-const int Sortie_PWM = 11;				// pin pour la sortie PWM, cette sortie 11 est adaptée car elle peut être utilisée pour du PWM
-// Ce qui sera déclenché si V < 1
-const int NF_220_24 = 12;						// pin pour le NF_220 et NF_24
-const int Buzzer_Lampe_24_MOC_304X_3 = 13;		// pin pour le Buzzer, la Lampe_24, et le MOC 304X 3
-
-//pratique
-const int Sec=1000; //Une variable qui me permet de compter en secondes dans le reste du programme :)
-
+// ===== Ce qui sera déclenché si V < 1
+int NF230      = 8;		// pin pour le MOC NF230V
+int LP230      = 9;		// pin pour le MOC LAMPE AC
+int Buzzer     = 10;	// pin pour le Buzzer
+int NF24       = 11;	// pin pour le 4N35 NF 24V
+int LEDrg      = 12;	// pin pour la LED rouge
 
 void setup()
 {
-	for(int i=10; i<14; i++)
-	{ // initialisation de l'ensemble des pins liés en sortie (sauf pour V_entrée)
-	    pinMode(i, OUTPUT);
-	    digitalWrite(i, LOW);
+	for(int i=2; i<13; i++){
+		pinMode(i, OUTPUT);			// initialisation de l'ensemble des pins sortantes NO (sauf pour A0 et A1)
+		digitalWrite(i, LOW);
 	}
-	pinMode(No220_24, OUTPUT);
-	digitalWrite(No220_24, LOW);
-	pinMode(Entree_V, INPUT); 					// initialisation du matos d'entrée qui déclenche le reste et simule l'entrée en Volts
-	pinMode(Potar_PWM, INPUT);					// initialisation du potar utilisé pour régler le PWM
-	V_entree, Entree_reelle, Valeur_PWM = 0;
+	
+	// ===== Allumage des Leds pour marquer le début du setup()
+	digitalWrite(LEDvt, HIGH);
+	digitalWrite(LEDrg, HIGH);
+
+
+	pinMode(Entree_V, INPUT);		// initialisation du matos d'entrée qui déclenche le reste et simule l'entrée en Volts
+	pinMode(Entree_Pot, INPUT);		// initialisation du potar utilisé pour régler le PWM
+	V_entree, Entree_reelle = 0; 
+	Valeur_PWM = 0;
+
+	// ===== Extinction des Leds pour marquer la fin du setup()
+	digitalWrite(LEDvt, LOW);		// Cela permet en même temps de visualiser le bon fonctionnement des LED
+	digitalWrite(LEDrg, LOW);
 }
 
 void loop()
 {
+	LectureEntreeVolts();
+	ChoixDesFonctions();
+}
+
+void LectureEntreeVolts(){
 	Entree_reelle = analogRead(Entree_V);
-	V_entree = map(Entree_reelle, 0, 1023, 0, 186); // On mappe l'entrée électrique lu analogiquement entre 0 et 186 (pour simuler le rapport entre 0 et 1,86V)
-	if(V_entree < 100)
-	{		// en dessous de 100 (1V, cf. le mappage de 0 à 186), on a la première fonction
-	    Zero_a_Un();
+	V_entree = map(Entree_reelle, 0, 1023, 0, 5000); 
+}
+
+void ChoixDesFonctions(){
+	// ===== au dessus de 1V, on appelle la deuxième fonction
+	if(V_entree > 1000){	 
+		delay(700); 			// timelaps pour éviter un phénomène du ballaste
+		Un_et_Plus();
 	}
-	if(V_entree >= 100)
-	{		// au dessus de 100 (1V), on appelle la deuxième fonction
-	    Un_et_Plus();
+	// ===== au dessous de 1V, on appelle la première fonction
+	else{
+		Zero_a_Un();
 	}
-	delay(10);
 }
 
 void Zero_a_Un()
 {
-	digitalWrite(No220_24, LOW);
-	digitalWrite(Lampe_Compteur_220, LOW);
-	digitalWrite(Sortie_PWM, LOW);
-
-	digitalWrite(NF_220_24, HIGH);
-	digitalWrite(Buzzer_Lampe_24_MOC_304X_3, HIGH);
+	digitalWrite(Compt_LP, LOW); 					// on coupe le compteur
+	digitalWrite(No230, LOW); 						// on coupe un premier relai 230VAC via un MOC3041
+	digitalWrite(LEDvt, LOW); 						// on coupe la led verte
+	analogWrite(Sortie_PWM, 0); 					// signal sortant de la sortie PWM à 0
+	digitalWrite(No24, LOW); 						// on coupe un premier relai 24VDC via un 4N35
+	digitalWrite(Gnd, LOW); 						// on coupe le Gnd du ventilo et du bandeau de leds via un BC337
+	
+	digitalWrite(Buzzer, HIGH); 					// on allume le buzzer
+	digitalWrite(NF24, HIGH); 						// on actionne un deuxième relai 24VDC via un 4N35
+	digitalWrite(LEDrg, HIGH); 						// on allume la led rouge
+	digitalWrite(NF230, HIGH); 						// on actionne un deuxième relai 230VAC via un MOC3041
+	digitalWrite(LP230, HIGH); 						// on actionne une lampe 230V via un MOC3041
 }
 
 void Un_et_Plus()
 {
-	digitalWrite(No220_24, HIGH);
-	digitalWrite(Lampe_Compteur_220, HIGH);
-	Valeur_PWM = analogRead(Potar_PWM);
-	analogWrite(Sortie_PWM, Valeur_PWM/4);
+	digitalWrite(Compt_LP, HIGH); 					// on alimente le compteur horaire via un MOC3041
+	digitalWrite(No230, HIGH); 						// on actionne le premier relai 230VAC via le MOC3041
+	digitalWrite(LEDvt, HIGH); 						// on allume la led verte
 
-	digitalWrite(NF_220_24, LOW);
-	digitalWrite(Buzzer_Lampe_24_MOC_304X_3, LOW);
+	Valeur_PWM = analogRead(Entree_Pot); 			// lecture de la tension sur A1 réglée par le potard
+	Valeur_PWM = map(Valeur_PWM, 0, 1023, 0, 255);		// map de la tension - de 0 à 5V en entrée donnent 0 à 255 en signal PWM
+	analogWrite(Sortie_PWM, Valeur_PWM); 			// cmde PWM envoyée sur Sortie_PWM de 0 à 255 - signaux carrés envoyés sur un 4N35
+
+	digitalWrite(No24, HIGH); 						// on actionne le premier relai 24VDC via le 4N35
+	digitalWrite(Gnd, HIGH); 						// on ferme le circuit du Gnd du ventilo et du bandeau de leds via un BC337
+
+	digitalWrite(Buzzer, LOW); 						// on coupe le buzzer
+	digitalWrite(NF24, LOW); 						// on ouvre le deuxième relai 24VDC via le 4N35
+	digitalWrite(LEDrg, LOW); 						// on coupe la led rouge
+	digitalWrite(NF230, LOW); 						// on ouvre le deuxième relai 230VAC via le MOC3041
+	digitalWrite(LP230, LOW); 						// on coupe la lampe 230VAC via le MOC3041
 }
